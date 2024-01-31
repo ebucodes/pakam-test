419d7e464c79d771448d0f75788383cd6b946761
Chukwuebuka Ohaji <ebucodes@gmail.com>
2024-01-31 09:43:52 +0100

    database migration

--- api/database/migrations/2024_01_31_082444_create_assessments_table.php
+++ api/database/migrations/2024_01_31_082444_create_assessments_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('assessments', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('quantity');
             $table->timestamps();
         });
     }
